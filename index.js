046209e2d422c194c8ef46836a68396accb1e9d9
emm12ng <nguyenthaiminhtrang@gmail.com>
2022-06-09 19:04:18 -0400

    6/9

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT  = 4000;
+
+// mongoose connection
+mongoose.Promise = global.Promise; // wait for result when connecting to MongoDB
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// body parser setup
+// allowing parser to parse requests coming in so that they are readable by API
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+
+routes(app);
 
 app.get('/', (req, res) => {
     res.send(`Node and express server running on port ${PORT}`)
